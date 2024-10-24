90558e53049d0511bb5d662dc6bb9844fd6a12fa
zdeckert <zdeckert@gmail.com>
2024-10-23 18:06:31 -0700

    implament counter gui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    let count = $state(0);
+    function incramentCount() {
+        count++;
+    }
+</script>
+
+<h1>Svelte 7 guis</h1>
+<h2>counter</h2>
+<label for="count">{count}</label>
+<button id="count" onclick={incramentCount}>Count</button>
