4edce1dd36f9ca4a1a73c3d4f3777e9b0f0aacc8
zdeckert <zdeckert@gmail.com>
2024-10-26 06:44:52 -0700

    implement flight booker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,16 +16,29 @@
 		if (fahrenheit === null) return;
 		celcius = Math.round((fahrenheit - 32) * (5 / 9));
 	}
-	$inspect(celcius, fahrenheit);
+
+	// Flight Booker
+	let flightType = $state('one_way');
+	let departureDate = $state(new Date().toISOString().slice(0, 10));
+	let returnDate = $state(new Date().toISOString().slice(0, 10));
+	function onSubmit() {
+		let msg =
+			flightType === 'one_way'
+				? `You booked a one-way flight on ${departureDate}`
+				: `You booked a round trip flight departing on ${departureDate} and returning ${departureDate}`;
+		alert(msg);
+	}
 </script>
 
 <h1>Svelte 7 guis</h1>
 
-<h2>counter</h2>
-<label for="count">{count}</label>
-<button id="count" onclick={incramentCount}>Count</button>
+<form id="counter">
+	<h2>counter</h2>
+	<label for="count">{count}</label>
+	<button id="count" onclick={incramentCount}>Count</button>
+</form>
 
-<form>
+<form id="temperature_converter">
 	<h2>Temperature Converter</h2>
 	<input
 		id="celcius"
@@ -45,5 +58,39 @@
 		bind:value={fahrenheit}
 		onchange={fahrenheitOnChange}
 	/>
-	C:{celcius} F:{fahrenheit}
 </form>
+
+<form id="flight_booker" onsubmit={onSubmit}>
+	<h2>Flight booker</h2>
+	<select bind:value={flightType} id="flight_select">
+		<option value="one_way">one way flight</option>
+		<option value="return">return flight</option>
+	</select>
+
+	<!-- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/date#additional_attributes
+	using new Date().toISOString().slice(0, 10) to match 'min' atrribute format
+	of a yyyy-mm-dd string.  -->
+	<input
+		id="departure_date"
+		type="date"
+		min={new Date().toISOString().slice(0, 10)}
+		bind:value={departureDate}
+	/>
+	<input
+		id="return_date"
+		type="date"
+		min={departureDate}
+		disabled={flightType === 'one_way'}
+		bind:value={returnDate}
+	/>
+	<button type="submit">Book</button>
+</form>
+
+<style>
+	#flight_booker {
+		display: flex;
+		flex-direction: column;
+		width: fit-content;
+		gap: 0.25rem;
+	}
+</style>
