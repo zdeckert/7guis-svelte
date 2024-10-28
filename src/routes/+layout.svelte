e15c838612786403c314e8c80bc748db8fbb5222
zdeckert <zdeckert@gmail.com>
2024-10-27 17:43:05 -0700

    circle-drawer wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,17 @@
 </script>
 
 <nav>
-	<h1><a href="/">🏠</a> Svelte 7 guis</h1>
-
+	<h1>
+		<a href="/">🏠</a>
+		Svelte 7 guis
+	</h1>
 	<ol>
 		<li><a href="/counter">counter</a></li>
 		<li><a href="/temperature-converter">temperature converter</a></li>
 		<li><a href="/flight-booker">flight booker</a></li>
 		<li><a href="/timer">timer</a></li>
 		<li><a href="/crud">crud</a></li>
+		<li><a href="/circle-drawer">circle drawer</a></li>
 	</ol>
 </nav>
 <slot />
---
+++ src/routes/circle-drawer/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	type Circle = {
+		cx: number;
+		cy: number;
+		r: number;
+	};
+
+	let stack;
+	let circles: Circle[] = $state([]);
+	function canvasOnClick() {}
+</script>
+
+<div class="flex-col">
+	<h2>Circle Drawer</h2>
+	<div class="flex">
+		<button id="undo">undo</button>
+		<button id="redo">redo</button>
+	</div>
+	<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
+	<svg
+		viewBox="0 0 300 200"
+		width="300"
+		height="200"
+		xmlns="http://www.w3.org/2000/svg"
+		onclick={canvasOnClick}
+	>
+		<circle cx="50" cy="50" r="50" stroke="black" stroke-width="2" fill="none" />
+	</svg>
+</div>
+
+<style>
+	svg {
+		border: 1px solid black;
+	}
+</style>
